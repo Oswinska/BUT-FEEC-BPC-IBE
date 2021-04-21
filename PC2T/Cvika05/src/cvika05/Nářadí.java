742342c1441fe0fed9f752d81daf78bc9024b14c
Oswin <xtoman41@stud.feec.vutbr.cz>
2021-04-21 05:58:20 +0200

    Update Nářadí.java

--- PC2T/Cvika05/src/cvika05/Nářadí.java
+++ PC2T/Cvika05/src/cvika05/Nářadí.java
@@ -1,4 +1,5 @@
 package cvika05;
+// Property of Oswin™
 
 public class Nářadí extends Zboží // Třída nářadí rozšiřující Zboží, v podstatě uplně to samé jak Potravina akorát místo expirace řešíme Záruku
 {
